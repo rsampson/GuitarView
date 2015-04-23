2781bf1a6c9da51a012b10e931a2c58f09f76518
richard <coolwonk@gmail.com>
2015-04-23 07:34:05 -0700

    Added some non-functional experimental code that may be used to improve fingering selection algorithm.

--- src/GuitarView/GuitarChannel.java
+++ src/GuitarView/GuitarChannel.java
@@ -2,6 +2,8 @@ package GuitarView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import processing.core.PApplet;
 
 // corresponds to a single midi channel played on a guitar
@@ -12,21 +14,33 @@ public class GuitarChannel {
 	private int lastStringPlayed;
 	private int track;
 	private int channel;
-	private FingerMarker[] stringStates;
-    //private static controlP5.Toggle parent;
+	private List<FingerMarker> stringStates;
     
-	GuitarChannel(List<controlP5.Toggle> parent, int chan, int track){
+
+	GuitarChannel(List<controlP5.Toggle> togl, int chan, int track){
 		channels.add(chan);
 		this.track = track;
 		channel = chan;
-        parent.add( GuitarView.cp5.addToggle("chan" + chan)
-				.setPosition(70 + (chans * 40), 60)
+		stringStates = new CopyOnWriteArrayList<FingerMarker>();
+		
+//        for (int s = 0; s < GuitarView.NUM_STRINGS; s++) {
+//        	FingerMarker fm = new FingerMarker(null, GuitarView.guitarImage, GuitarView.strings[s], 0);
+//        	fm.setColor(FingerMarker.invisible);
+//        	stringStates.add(fm);
+//        }
+        
+        togl.add( GuitarView.cp5.addToggle("chan" + chan)
+				.setPosition((5 + chans) * GuitarView.dX, 2 * GuitarView.dY)
 				.setSize(20, 20)
 				.setValue(true)
 				.setColorActive(FingerMarker.codeColor(chan)));
         chans++;
 	}
 	
+	public List<FingerMarker> getStringStates() {
+		return stringStates;
+	}
+
 	public static boolean isChannelEnabled(List<controlP5.Toggle> parent, int channel) {
 	  return parent.get(channels.indexOf(channel)).getState() == true;
 	}
@@ -59,14 +73,6 @@ public class GuitarChannel {
 		this.lastStringPlayed = lastStringPlayed;
 	}
 
-	public FingerMarker[] getStringStates() {
-		return stringStates;
-	}
-
-	public void setStringStates(FingerMarker[] stringStates) {
-		this.stringStates = stringStates;
-	}
-
 	public static int getIndexOfChannel(int channel) {
 	  return channels.indexOf(channel);
 	}
@@ -77,7 +83,9 @@ public class GuitarChannel {
 		float shortestLength = 9999999;
 		// for each fingering pair in sf
 		for (int i = 0; i < sf.size() / 2; i = i + 2) {
-			length = /* PApplet.sqrt */ (2 *(PApplet.sq(sf.get(i) - lastStringPlayed))
+			// "length" is not a measure in cartesian space, but a measure of difficulty in
+			// reaching a note from another position.
+			length = /* PApplet.sqrt */ ((float)1.5 * (PApplet.sq(sf.get(i) - lastStringPlayed))
 					+ PApplet.sq(sf.get(i + 1) - lastFretPlayed));
 			if (length < shortestLength) {
 				shortestLength = length;
