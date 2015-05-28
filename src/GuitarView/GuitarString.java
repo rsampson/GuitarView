99090178cf5949c8d7938321d030b672e6770182
rsampson <coolwonk@gmail.com>
2015-05-28 07:08:30 -0700

    Pass in color so that the strings can be drawn in color.

--- src/GuitarView/GuitarString.java
+++ src/GuitarView/GuitarString.java
@@ -4,12 +4,10 @@ import processing.core.PGraphics;
 
 public class GuitarString {
 	private int y;
-	private PGraphics pg;
 	private GuitarView vg;
 
-	GuitarString(GuitarView vg, PGraphics pg, int y) {
+	GuitarString(GuitarView vg, int y) {
 		this.y = y;
-		this.pg = pg;
 		this.vg = vg;
 	}
 
@@ -17,15 +15,14 @@ public class GuitarString {
 		return y;
 	}
 
-	public void draw() {
+	public void draw(PGraphics pg, int color) {
 		pg.strokeWeight(3);
-		pg.fill(vg.copper);
-		pg.stroke(vg.copper);
+		pg.fill(color);
+		pg.stroke(color);
 		pg.line(GuitarView.guitarX, y, vg.width, y);
 		// draw shadow
 		pg.fill(10, 10, 3);
 		pg.stroke(10, 10, 3);
 		pg.line(GuitarView.guitarX, y + 3, vg.width, y + 3);
-
 	}
 }
